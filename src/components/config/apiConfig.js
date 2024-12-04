234a5fa02349c9317312d0f8e04accebc158e4cd
Udeme01 <emmanueluddy2@gmail.com>
2024-12-04 12:24:04 +0100

    added gitignore

--- src/components/config/apiConfig.js
+++ src/components/config/apiConfig.js
@@ -1,8 +1,8 @@
 // src/config/apiConfig.js
 
-const spaceId = "if5o0uzuywbh"; // Replace with your Contentful space ID
-const environment = "master"; // Replace with your environment (default: master)
-const accessToken = "WKDh2DUCsUwONv29xdvVyM7U6eNR6KsHfdJJFcNY9eE"; // Replace with your Contentful access token
+const spaceId = import.meta.env.VITE_CONTENTFUL_SPACE_ID; // Replace with your Contentful space ID
+const environment = import.meta.env.VITE_CONTENTFUL_ENVIRONMENT; // Replace with your environment (default: master)
+const accessToken = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN; // Replace with your Contentful access token
 
 const apiConfig = {
   baseUrl: `https://cdn.contentful.com/spaces/${spaceId}/environments/${environment}`,
